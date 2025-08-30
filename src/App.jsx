8d5a6010170bf2bf95007ad6000b0e56c480b6cd
christian ventes <chrventes@gmail.com>
2025-08-29 22:32:29 -0500

    primer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
-
+import React, { useState } from 'react';
 import './App.css'
 
 function App() {
+   const [count, setCount] = useState(0);
+   const increment = () => setCount(count + 1);
   
 
   return (
     <>
-     <h1>Hoy veremos sobre interacciones ddd</h1>
+     <h1>Hoy veremos sobre interacciones </h1>
+      <div>
+      <p>Contador: {count}</p>
+      <button onClick={increment}>Incrementar</button>
+    </div>
     </>
   )
 }
